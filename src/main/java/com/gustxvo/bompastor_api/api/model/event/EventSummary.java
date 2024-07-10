59de0a286c39ddc291652ea3c429f6a5228ffe6c
gustxvo <gualmeida2004@gmail.com>
2024-07-10 16:59:45 -0300

    dev: Add mock data for sector response

--- src/main/java/com/gustxvo/bompastor_api/api/model/event/EventSummary.java
+++ src/main/java/com/gustxvo/bompastor_api/api/model/event/EventSummary.java
@@ -1,14 +1,16 @@
 package com.gustxvo.bompastor_api.api.model.event;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.gustxvo.bompastor_api.api.model.sector.SectorSummary;
 import com.gustxvo.bompastor_api.domain.model.event.Event;
 
 import java.time.LocalDateTime;
 
-public record EventSummary(@JsonProperty("event_id") Long eventId, @JsonProperty("sector_name") String sectorName,
+public record EventSummary(@JsonProperty("event_id") Long eventId, SectorSummary sector,
                            @JsonProperty("date_time") LocalDateTime dateTime) {
 
     public static EventSummary fromEntity(Event event) {
-        return new EventSummary(event.getId(), event.getSector().getName(), event.getDateTime());
+        return new EventSummary(event.getId(),
+                SectorSummary.fromEntity(event.getSector()),event.getDateTime());
     }
 }
