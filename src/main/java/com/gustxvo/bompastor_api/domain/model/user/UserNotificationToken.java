ba1142ae7ac0f3fa275f992b2f1073a230370891
gustxvo <gualmeida2004@gmail.com>
2024-07-21 22:50:37 -0300

    refactor: API adjustments

--- src/main/java/com/gustxvo/bompastor_api/domain/model/user/UserNotificationToken.java
+++ src/main/java/com/gustxvo/bompastor_api/domain/model/user/UserNotificationToken.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 public class UserNotificationToken {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "device_id")
     private Long deviceId;
 
