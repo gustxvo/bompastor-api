e949430083136ce43bdd20f2d644771c4c511a82
gustxvo <gualmeida2004@gmail.com>
2024-07-27 23:03:18 -0300

    core: remove fake users

--- src/main/resources/db/migration/V002__prepopulate_sectors.sql
+++ src/main/resources/db/migration/V002__prepopulate_sectors.sql
@@ -1,25 +1,12 @@
-INSERT INTO tb_user (user_id, name, email, password, role) VALUES
-    ('00b7b810-9dad-11d1-80b4-00c04fd430c8', 'Vanessa Calheiros', 'vanessacalheiros@bp.com', '$2a$10$Y2zVfNQP94ZCxAJnV3.4Y.xLpmnx1fhgSmlTzxyHzn.brrS6/cgbq', '1'),
-    ('01b7b810-9dad-11d1-80b4-00c04fd430c8', 'João Silva', 'joaosilva@bp.com', '$2a$10$Y2zVfNQP94ZCxAJnV3.4Y.xLpmnx1fhgSmlTzxyHzn.brrS6/cgbq', '1'),
-    ('02b7b810-9dad-11d1-80b4-00c04fd430c8', 'Ana Oliveira', 'anaoliveira@bp.com', '$2a$10$Y2zVfNQP94ZCxAJnV3.4Y.xLpmnx1fhgSmlTzxyHzn.brrS6/cgbq', '1'),
-    ('03b7b810-9dad-11d1-80b4-00c04fd430c8', 'Pedro Santos', 'pedrosantos@bp.com', '$2a$10$Y2zVfNQP94ZCxAJnV3.4Y.xLpmnx1fhgSmlTzxyHzn.brrS6/cgbq', '1'),
-    ('04b7b810-9dad-11d1-80b4-00c04fd430c8', 'Maria Costa', 'mariacosta@bp.com', '$2a$10$Y2zVfNQP94ZCxAJnV3.4Y.xLpmnx1fhgSmlTzxyHzn.brrS6/cgbq', '1'),
-    ('05b7b810-9dad-11d1-80b4-00c04fd430c8', 'Carlos Pereira', 'carlospereira@bp.com', '$2a$10$Y2zVfNQP94ZCxAJnV3.4Y.xLpmnx1fhgSmlTzxyHzn.brrS6/cgbq', '1'),
-    ('06b7b810-9dad-11d1-80b4-00c04fd430c8', 'Sofia Ferreira', 'sofiaferreira@bp.com', '$2a$10$Y2zVfNQP94ZCxAJnV3.4Y.xLpmnx1fhgSmlTzxyHzn.brrS6/cgbq', '1'),
-    ('07b7b810-9dad-11d1-80b4-00c04fd430c8', 'Jorge Martins', 'jorgemartins@bp.com', '$2a$10$Y2zVfNQP94ZCxAJnV3.4Y.xLpmnx1fhgSmlTzxyHzn.brrS6/cgbq', '1'),
-    ('08b7b810-9dad-11d1-80b4-00c04fd430c8', 'Margarida Rodrigues', 'margaridarodrigues@bp.com', '$2a$10$Y2zVfNQP94ZCxAJnV3.4Y.xLpmnx1fhgSmlTzxyHzn.brrS6/cgbq', '1'),
-    ('09b7b810-9dad-11d1-80b4-00c04fd430c8', 'Rita Sousa', 'ritasousa@bp.com', '$2a$10$Y2zVfNQP94ZCxAJnV3.4Y.xLpmnx1fhgSmlTzxyHzn.brrS6/cgbq', '1'),
-    ('10b7b810-9dad-11d1-80b4-00c04fd430c8', 'Manuel Gonçalves', 'manuelgoncalves@bp.com', '$2a$10$Y2zVfNQP94ZCxAJnV3.4Y.xLpmnx1fhgSmlTzxyHzn.brrS6/cgbq', '1');
-
-INSERT INTO tb_sector (leader_id, name) VALUES
-    ('00b7b810-9dad-11d1-80b4-00c04fd430c8', 'Consolidação'),
-    ('01b7b810-9dad-11d1-80b4-00c04fd430c8', 'Ceia'),
-    ('02b7b810-9dad-11d1-80b4-00c04fd430c8', 'Cenografia'),
-    ('03b7b810-9dad-11d1-80b4-00c04fd430c8', 'Estudos'),
-    ('04b7b810-9dad-11d1-80b4-00c04fd430c8', 'Intercessão'),
-    ('05b7b810-9dad-11d1-80b4-00c04fd430c8', 'Louvor'),
-    ('06b7b810-9dad-11d1-80b4-00c04fd430c8', 'Mídia'),
-    ('07b7b810-9dad-11d1-80b4-00c04fd430c8', 'Kids'),
-    ('08b7b810-9dad-11d1-80b4-00c04fd430c8', 'Organização'),
-    ('09b7b810-9dad-11d1-80b4-00c04fd430c8', 'Recepção'),
-    ('10b7b810-9dad-11d1-80b4-00c04fd430c8', 'Projetor');
+INSERT INTO tb_sector (name) VALUES
+    ('Consolidação'),
+    ('Ceia'),
+    ('Cenografia'),
+    ('Estudos'),
+    ('Intercessão'),
+    ('Louvor'),
+    ('Mídia'),
+    ('Kids'),
+    ('Organização'),
+    ('Recepção'),
+    ('Projetor');
